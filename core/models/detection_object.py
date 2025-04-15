653c3a26443f4f377563d98e04ab2a23a375c35b
stephenprn <stephen.perrin@2017.icam.fr>
2025-04-15 11:39:23 +0200

    feat: add commune_id fk to detection model

--- core/models/detection_object.py
+++ core/models/detection_object.py
@@ -7,6 +7,7 @@ from common.models.historied import HistoriedModelMixin
 from common.models.importable import ImportableModelMixin
 from common.models.timestamped import TimestampedModelMixin
 from common.models.uuid import UuidModelMixin
+from core.models.geo_commune import GeoCommune
 from core.models.geo_custom_zone import GeoCustomZone
 from core.models.object_type import ObjectType
 from core.models.parcel import Parcel
@@ -28,7 +29,14 @@ class DetectionObject(
         related_name="detection_objects",
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+    )
+    commune = models.ForeignKey(
+        GeoCommune,
+        related_name="detection_objects",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
     )
     geo_custom_zones = models.ManyToManyField(
         GeoCustomZone, related_name="detection_objects"
