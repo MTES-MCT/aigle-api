1cf65374b08dc5fcc31e107f67de4b33988e4182
stephenprn <stephen.perrin@2017.icam.fr>
2025-05-09 13:16:49 +0200

    tech: fix deployment-infos endpoint

--- core/views/utils/deploy_infos.py
+++ core/views/utils/deploy_infos.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from aigle.settings import DEPLOYMENT_DATETIME
 
 
-def endpoint():
+def endpoint(request):
     return JsonResponse({"datetime": str(DEPLOYMENT_DATETIME)})
 
 
