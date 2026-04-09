fb6780d20e316083200ced7630bafef449116325
stephenprn <stephen.perrin@2017.icam.fr>
2026-04-09 18:07:03 +0200

    tech: improve get-custom-geometry endpoint perfs

--- core/views/utils/get_custom_geometry.py
+++ core/views/utils/get_custom_geometry.py
@@ -9,13 +9,15 @@ from core.constants.geo import SRID
 from core.constants.order_by import GEO_CUSTOM_ZONES_ORDER_BYS
 from core.models.geo_custom_zone import GeoCustomZone, GeoCustomZoneStatus
 from core.serializers.geo_custom_zone import GeoCustomZoneGeoFeatureSerializer
+from core.utils.postgis import SimplifyPreserveTopology
 from django.contrib.gis.geos import Polygon
 from django.contrib.gis.db.models.functions import Intersection
 from django.contrib.gis.db.models.aggregates import Union
-from django.contrib.gis.geos import GEOSGeometry
 from django.db.models.functions import Coalesce
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+
+SIMPLIFY_TOLERANCE = 0.0001
 
 
 class GeometrySerializer(serializers.Serializer):
@@ -54,32 +56,50 @@ def endpoint(request):
 
         custom_zones = queryset.all()
 
-    geometry_negative = []
+    geometry_negative = None
 
     if geometry_serializer.data.get("uuidsNegative"):
-        queryset_covered = get_queryset_geocustomzone(
+        geometry_negative = get_negative_geometry(
             uuids=geometry_serializer.data["uuidsNegative"].split(","),
             polygon_requested=polygon_requested,
         )
-        geometry_covered = queryset_covered.aggregate(union_geometry=Union("geometry"))[
-            "union_geometry"
-        ]
-
-        if geometry_covered:
-            geometry_negative = polygon_requested.difference(geometry_covered)
-        else:
-            geometry_negative = polygon_requested
 
     return JsonResponse(
         {
             "customZones": GeoCustomZoneGeoFeatureSerializer(
                 custom_zones, many=True
             ).data,
-            "customZoneNegative": json.loads(GEOSGeometry(geometry_negative).geojson)
+            "customZoneNegative": json.loads(geometry_negative.geojson)
             if geometry_negative
             else None,
         }
     )
+
+
+def get_negative_geometry(uuids: List[str], polygon_requested: Polygon):
+    queryset = GeoCustomZone.objects.filter(
+        geo_custom_zone_status=GeoCustomZoneStatus.ACTIVE,
+        geometry__intersects=polygon_requested,
+    )
+
+    try:
+        queryset = queryset.filter(uuid__in=uuids)
+    except (ValueError, TypeError):
+        return polygon_requested
+
+    geometry_covered = queryset.aggregate(
+        union_geometry=Union(Intersection("geometry", polygon_requested))
+    )["union_geometry"]
+
+    if not geometry_covered:
+        return polygon_requested
+
+    result = polygon_requested.difference(geometry_covered)
+
+    if not result or result.empty:
+        return None
+
+    return result.simplify(SIMPLIFY_TOLERANCE, preserve_topology=True)
 
 
 def get_queryset_geocustomzone(uuids: List[str], polygon_requested: Polygon):
@@ -88,18 +108,21 @@ def get_queryset_geocustomzone(uuids: List[str], polygon_requested: Polygon):
     try:
         queryset = queryset.filter(uuid__in=uuids)
     except (ValueError, TypeError):
-        # Invalid UUID format
         pass
 
     queryset = queryset.filter(geo_custom_zone_status=GeoCustomZoneStatus.ACTIVE)
     queryset = queryset.filter(geometry__intersects=polygon_requested)
-    queryset = queryset.select_related("geo_custom_zone_category")
     queryset = queryset.values("uuid", "geo_custom_zone_status", "geo_custom_zone_type")
     queryset = queryset.annotate(
         name=Coalesce("name", "geo_custom_zone_category__name"),
         color=Coalesce("color", "geo_custom_zone_category__color"),
     )
-    queryset = queryset.annotate(geometry=Intersection("geometry", polygon_requested))
+    queryset = queryset.annotate(
+        geometry=SimplifyPreserveTopology(
+            Intersection("geometry", polygon_requested),
+            SIMPLIFY_TOLERANCE,
+        )
+    )
 
     return queryset
 
