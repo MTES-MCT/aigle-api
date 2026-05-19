a223eafe873f3cca0c5a789ef6f028c330f8b285
stephenprn <stephen.perrin@2017.icam.fr>
2026-05-19 11:09:11 +0200

    fix: SUPER_ADMIN rights

--- core/services/tile_set.py
+++ core/services/tile_set.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 
 from core.models.tile_set import TileSet, TileSetType
+from core.models.user_group import UserGroup
 from core.permissions.tile_set import TileSetPermission
 from django.contrib.gis.geos import Point
 
@@ -10,13 +11,17 @@ class TileSetService:
 
     @staticmethod
     def find_tile_set_by_coordinates(
-        x: float, y: float, user, tile_set_types: Optional[List[TileSetType]] = None
+        x: float,
+        y: float,
+        user,
+        tile_set_types: Optional[List[TileSetType]] = None,
+        scoped_user_group: Optional[UserGroup] = None,
     ) -> Optional[TileSet]:
         """Find the appropriate tile set for given coordinates with user permissions."""
         point = Point(x, y)
 
         return (
-            TileSetPermission(user=user)
+            TileSetPermission(user=user, scoped_user_group=scoped_user_group)
             .filter_(
                 filter_tile_set_type_in=tile_set_types,
                 order_bys=["-date"],
